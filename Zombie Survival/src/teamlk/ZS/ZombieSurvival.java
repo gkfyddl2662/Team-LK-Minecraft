3152fa2df1fee3241209fdd78a4a072c495fc592
Administrator <Administrator@192.168.0.136>
2015-02-01 00:14:09 +0900

    Team LK

---
+++ Zombie Survival/src/teamlk/ZS/ZombieSurvival.java
@@ -0,0 +1,10 @@
+package teamlk.ZS;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ZombieSurvival extends JavaPlugin implements Listener {
+	
+	public void onEnable()
+
+}
